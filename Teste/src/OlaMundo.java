583415d1b81c4ac471c46e653b6234c8a8a35e39
Tércio Ribeiro <Trcio Ribeiro@Trcio.mshome.net>
2018-05-18 10:58:28 -0300

    Teste Git

---
+++ Teste/src/OlaMundo.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class OlaMundo {
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo!");
+		System.out.println("Tercio");
+		Date relogio = new Date();
+		System.out.println(relogio);
+		
+		
+	}
+}
